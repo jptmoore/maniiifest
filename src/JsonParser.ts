17bbeb1ed6371d5e664f4198347e819c7329e108
John Moore <jmoore@zedstar.org>
2024-06-21 17:43:59 +0100

    experiment with decorators

--- src/JsonParser.ts
+++ src/JsonParser.ts
@@ -2,7 +2,28 @@ import fs from 'fs';
 import * as spec from "./specification"
 import { TargetT } from './specification';
 
+function DataWrapper(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function(...args: any[]) {
+        const result = originalMethod.apply(this, args);
+
+        if (typeof result === 'string' && result.length > 0) {
+            return ["T1", result];
+        } 
+
+        if (result && typeof result === 'object') {
+            return ["T2", result];
+        } 
+
+        return result;
+    }
+
+    return descriptor;
+}
+
 export class JsonParser {
+    @DataWrapper
     static readJsonFromFile(filename: string): any {
         try {
             const rawData = fs.readFileSync(filename, 'utf-8');
