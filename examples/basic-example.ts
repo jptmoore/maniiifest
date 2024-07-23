d10ead624a82de29855d832f3dc811fba90449a6
John Moore <jmoore@zedstar.org>
2024-07-23 21:04:03 +0100

    make more library ready

---
+++ examples/basic-example.ts
@@ -0,0 +1,29 @@
+import { readJsonFromFile } from '../src/utils'
+import { Manifesty } from '../src/Manifesty';
+
+const filename = 'test/detailed.json';
+
+(function () {
+    try {
+        const jsonData = readJsonFromFile(filename);
+        if (!jsonData) {
+            console.error("No data returned from the file.");
+            return;
+        }
+
+        const parser = new Manifesty(jsonData);
+
+        for (const item of parser.iterateRendering()) {
+            console.log(item)
+        }
+
+        // const result = parser.getManifest();
+        // console.log(result);
+
+
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+})();
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,4 @@
-import { readJsonFromFile } from './utils'
 import { Manifesty } from './Manifesty';
 
-const filename = 'test/detailed.json';
-
-(function () {
-    try {
-        const jsonData = readJsonFromFile(filename);
-        if (!jsonData) {
-            console.error("No data returned from the file.");
-            return;
-        }
-
-        const parser = new Manifesty(jsonData);
-
-        for (const item of parser.iterateRendering()) {
-            console.log(item)
-        }
-
-        // const result = parser.getManifest();
-        // console.log(result);
-
-
-    } catch (error) {
-        console.error("An error occurred:", error);
-    }
-})();
-
+export { Manifesty };
 
