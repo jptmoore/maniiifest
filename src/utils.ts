a222171f40cee45d41fdf78076f13ac23baeb4f7
John Moore <jmoore@zedstar.org>
2026-04-08 21:35:37 +0100

    remove use of console log

--- src/utils.ts
+++ src/utils.ts
@@ -4,8 +4,7 @@ export function readJsonFromFile(filename: string): any {
     try {
         const rawData = fs.readFileSync(filename, 'utf-8');
         return JSON.parse(rawData);
-    } catch (error) {
-        console.error(`Error reading or parsing file: ${filename}`, error);
+    } catch {
         return null;
     }
 }
